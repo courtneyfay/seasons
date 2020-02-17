4995d8a967d011594f24a216d21187aeaa181902
Courtney Fay <fay.courtney@gmail.com>
2020-02-16 17:00:06 -0700

    changing out class based component for function w hooks

--- src/index.js
+++ src/index.js
@@ -1,56 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import useLocation from "./useLocation";
 
-import SeasonDisplay from './SeasonDisplay'; 
-import Spinner from './Spinner';
+const App = () => {
+  const [lat, errorMessage] = useLocation();
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request" />;
+  }
 
-class App extends React.Component {
-    //don't have to use constructor function!
-    state = { lat: null, errorMessage: '' };
-
-    componentDidMount() {
-        console.log('My component was rendered to the screen');
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude }),
-            err => this.setState({ errorMessage: err.message })
-        );
-    };
-
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: { this.state.errorMessage }</div>;
-        }
-
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={ this.state.lat } />
-        }
-
-        return <Spinner message="Please accept location request" />;
-    }
-
-    render() {
-       return (
-           <div className="border red">
-               { this.renderContent() }
-           </div>
-       )
-    }
-
-    componentDidUpdate() {
-        console.log('My component was just updated. It re-rendered.');
-    };
-
-    componentWillUnmount() {
-        console.log('Component removed from the screen, and it has been cleaned up.');
-    }
-    
-    // 3 other lifecycle methods that are used VERY infrequently
-    // shouldComponentUpdate
-    // getDerivedStateFromProps
-    // getSnapshotBeforeUpdate
+  return <div className="border red">{content}</div>;
 };
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
